7083277a79da8ffc575c6855beb89a20c43afa49
unknown <e.lucasrocha@gmail.com>
2020-11-10 11:32:31 -0300

    Retirada a parte de execução da main e correção de alguns bugs.

--- EPshell.c
+++ EPshell.c
@@ -6,6 +6,28 @@
 #define MAX_WORD 10
 #define MAX_CHAR 512 // tamanho maximo de entrada de caracteres
 
+void execucao(char* args[]){
+    if(strcmp(args[0], "quit")==0 || args[0] == NULL) {
+		exit(0);
+	}
+
+    pid_t pid = fork();
+
+    if(pid == -1) {
+        perror("Algo deu errado no fork...");
+    }
+    if(pid == 0) {
+        if(execvp(args[0], args) == -1){
+            perror("Erro na execução");
+            exit(1);
+        }
+    }
+
+    if(wait(0) == -1){
+        perror("Erro no wait");
+    }
+}
+
 void remove_endOfLine(char line[]) {
 	int i=0;
 	while(line[i] != '\n') // encontra a ultima posicao do que foi digitado
@@ -14,25 +36,10 @@ void remove_endOfLine(char line[]) {
 	line[i] = '\0'; // coloca a ultima posição como NULL
 }
 
-void read_line(char line[]) {
-	char* username = getenv("USER"); // pega o username para printar
-	printf("%s@EPshell> ", username);
-	char* input = fgets(line, MAX_CHAR, stdin); //gets estava dando warning
-	
-	remove_endOfLine(line);
-
-	if(strcmp(line, "quit")==0 || input == NULL) {
-		exit(0);
-	}
-    if(strlen(input)>=512){
-        printf("Ops... caracteres demais!");
-        exit(0);
-    }
-}
-
-int process_line(char* args[], char line[]) {
+int process_line(char line[]) {
 	int i = 0;
 	//args[i] = strtok(line," "); 
+    char* args[MAX_WORD];
     char *token = strtok(line, ",");
 
     while(token != NULL){
@@ -46,27 +53,30 @@ int process_line(char* args[], char line[]) {
         args[i] = NULL;
         token = strtok(NULL, ",");
     }
-	return 1;
+	execucao(args);
 }
 
-int read_parse_line(char* args[], char line[]) {
-	read_line(line);
-	process_line(args, line);
-	return 1;
+void read_line(char line[]) {
+	char* username = getenv("USER"); // pega o username para printar
+	printf("%s@EPshell> ", username);
+	char* input = fgets(line, MAX_CHAR, stdin); //gets estava dando warning
+	
+	remove_endOfLine(line);
+
+	
+    if(strlen(input)>=512){
+        printf("Ops... caracteres demais!");
+        exit(0);
+    }
+    process_line(line);
 }
 
 int main () {
 	char* args[MAX_WORD]; //comandos
 	char line[MAX_CHAR]; //entrada do usuario
 
-	while(read_parse_line(args,line)) {
-		pid_t child_pid = fork();
-
-		if(child_pid == 0) {
-			execvp(args[0], args);
-		}else {
-			waitpid(child_pid, 0);
-		}
-	}
+	while(1){
+        read_line(line);
+    }
 	return 0;
 }
